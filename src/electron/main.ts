fa041c553cbd4a9cf33977e7075fef9a659f6028
Daljeet Singh <daljeet.singh@icloud.com>
2024-10-15 02:50:55 -0700

    Electron Setup

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,9 +1,13 @@
 import { app, BrowserWindow } from 'electron';
-import * as path from "node:path";
+// import * as path from "node:path";
 
 app.on("ready", () => {
     const mainWindow = new BrowserWindow({});
-    mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+
+    // mainWindow.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+
+    // Dev Mode
+    mainWindow.loadURL("http://localhost:5173/");
 
     mainWindow.removeMenu();
 });
